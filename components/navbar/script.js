3e10aa5341aef89a1eb4f033373e586131980054
VictorLCosta <victorlc2019@outlook.com>
2021-11-29 11:52:54 -0300

    Commit

--- components/navbar/script.js
+++ components/navbar/script.js
@@ -1,9 +1,13 @@
 let nav = document.querySelector(".nav-links");
+let menuBtn = document.querySelector("#menu-btn");
 
-document.querySelector(".fa-bars").onclick = () => {
+menuBtn.onclick = () => {
     nav.classList.toggle("active")
+    menuBtn.classList.toggle("active")
+    menuBtn.classList.toggle("fa-times")
 }
 
 window.onscroll = () => {
     nav.classList.remove("active")
+    menuBtn.classList.remove("active")
 }
